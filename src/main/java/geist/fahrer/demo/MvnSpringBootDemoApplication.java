12b3d3789e7cf9949148e113939b1acb27eaed5c
Bishu Roy <biroy1393@gmail.com>
2022-05-08 23:14:52 +0530

    Initial Commit

---
+++ src/main/java/geist/fahrer/demo/MvnSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package geist.fahrer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/geist/fahrer/demo/MvnSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package geist.fahrer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
